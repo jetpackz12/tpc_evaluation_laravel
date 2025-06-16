7083d350b2782067de68c08e68c60bdea266f909
Jetpackz <jetpackzabela12@gmail.com>
2025-06-16 06:42:52 +0000

    feat/StudentStatusAndAccountStatus

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,23 +2,25 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
+    
+    use WithoutModelEvents;
+    
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Admin',
-            'username' => 'admin',
-            'password' => 'admin',
+        $this->call([
+            UserSeeder::class,
+            AccountStatusSeeder::class,
+            StudentStatusSeeder::class
         ]);
     }
 }
