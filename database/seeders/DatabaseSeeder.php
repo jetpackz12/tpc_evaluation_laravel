737e596e51dcbbecc89eb52732c41d5468b69b4f
Jetpackz <jetpackzabela12@gmail.com>
2025-06-17 01:19:37 +0000

    feat/Program

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,7 +21,8 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             AccountStatusSeeder::class,
             StudentStatusSeeder::class,
-            YearLevelSeeder::class
+            YearLevelSeeder::class,
+            ProgramSeeder::class
         ]);
     }
 }
